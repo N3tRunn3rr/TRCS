40f9e5cc0114e4311c339f24e869367f19894fc2
Kenji Fleming <kwf1999@gmail.com>
2023-06-05 12:36:14 -0400

    added testimonials

--- src/Components/About/Testimonials.js
+++ src/Components/About/Testimonials.js
@@ -14,34 +14,352 @@ export default class Testimonials extends Component {
         interval={6100}
       >
         <div>
-          <img src="/test1.png" alt="women test" />
           <div className="myCarousel">
-            <h3>Dorothy Szczepanski</h3>
-            <h4>Realtor</h4>
+            <h3>Tushar Shembekar</h3>
             <p>
-            It has been a delight to work with TRCS and the staff.The people at TRCS have been SO great with communication - keeping me advised of the progress of my purchase and answering any question I had.
+            Your company was awesome! I would definitely use you again !!
             </p>
           </div>
         </div>
 
         <div>
-          <img src="/test2.png" alt= "test2 man" />
           <div className="myCarousel">
-            <h3>Bob Curtin</h3>
-            <h4>Realtor</h4>
+            <h3>Sonny & Tita Bautista</h3>
             <p>
-            Thanks so much for your help with this closing. I would have no hesitation in recommending your services to anyone looking for assistance. It has been a very painless and straightforward experience for me. 
+            We wanted to thank you for providing my wife and I such a smooth, and trouble-free experience. From the time that the process started, to the time that our sale had closed, you and your team have been able to provide us with excellent communication. Thank you for helping us through this process. If any of our friends are ever in need of your services, we will not hesitate to recommend your company.
             </p>
           </div>
         </div>
 
         <div>
-          <img src="/test3.png" alt="test3 man" />
           <div className="myCarousel">
-            <h3>Roger Axe</h3>
-            <h4>Realtor</h4>
+            <h3>Mary and Al Santora</h3>
             <p>
-            Organizing a Timeshare closing from Australia filled me with horror but TRS Inc handled it all with very little input from me and told me step by step what was happening.
+            Selling a timeshare can be a stressful process. Thanks to the professionalism that you and your company exhibited, that stress disappeared. We received timely communications from beginning to end. The instructions regarding the necessary paperwork and completing the required forms were clear and concise. Thank you again for the service that you provided to us.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Charles W. Ward</h3>
+            <p>
+            I want to thank you and your associates for the fine work you did from listing of the time share to every aspect of the closing. We received the check yesterday. Everything was done in a timely and professional manner and questions were answered immediately. We could not have asked for better service.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Brenda Gerling</h3>
+            <p>
+            We received our check today. We were quite impressed with how efficient our closing was taken care of and would highly recommend your services. We had a much better experience with you than we did with the company we went through to list our timeshare. Because of that, we really didn't expect much better from you. BUT, much to our surprise, you not only met our our expectation but exceeded them by a mile!!! You did what you said you were going to do, when you said you were going to do it and did it in a timely manner. No lies, no frustrations, just the truth!!! We can't thank you enough!!!
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Curt Graham</h3>
+            <p>
+            We were Extremely Pleased with TRCS in the fact that you kept us informed every step of the way. We would not Hesitate to do Buisness with TRCS, INC in the Future. Thank you for all your help.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Mark and Kristi Novakovich</h3>
+            <p>
+            We appreciated the timely updates provided by TRCS. Not only did your online tool let us know where we were at in the process, but you were always willing to answer our e-mails in a timely and helpful manner. Thank you for your support and the high level of professionalism! I’m sure we are just one of many happy customers.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Dexter Freeman</h3>
+            <p>
+            This process has been extremely smooth and professional. I must admit it was a bit nerve racking—the thought of purchasing a timeshare and using an agent and closing company that you have never met. However, your company provided prompt and responsive services that quickly set my mind at ease. I look forward to our next vacation in Orlando, staying in our timeshare at Sea World. Thanks for your support in this process.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Carl & Katy Posluns</h3>
+            <p>
+            It has been a pleasure to use your services for purchasing this timeshare. Everything was handled with great efficiency and with excellent communication. Thanks again.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Nancy Cowen</h3>
+            <p>
+            Thank you for your promptness and professionalism during our closing process. The updates through your website gave us up-to-date information on the status and timing of our closing. TRCS Inc. was very professional. When our needs arise again we will look to you for assistance.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Andrea Artzr</h3>
+            <p>
+            Thank you for taking care of my time share closing and keeping me informed. I have been very satisfied with your services. Your responses to my emails have been timely and informative. Having the website to log in and monitor the status is a great feature. I look forward to my deed and being able to start reserving my vacation.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Mark Greenberg</h3>
+            <p>
+            You have been the best closer we have ever worked with - total communication, always clear, always nice. Really appreciate it.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Jeff McCabe</h3>
+            <p>
+            Very professional and competent on your part. Extremely smooth and fast escrow.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Lisa and.Richard Sadowski</h3>
+            <p>
+            I want to thank you for your professional, efficient closing on our new Hilton condo. You completed everything ahead of schedule and were very kind and courteous with all my questions. I'd definitely recommend your company to anyone going through a closing in the future. Our son, Brandon,.cannot wait to go visit his new vacation home near Universal and Disney.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Dan & Cindy DeSanzo</h3>
+            <p>
+            Just wanted to let you know how much we appreciated all your efforts in getting this closed for us so quick as well as your communications every step of the way. You and your company helped make this a stress free transaction for us.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>William A. Moore</h3>
+            <p>
+            My wife and I want to thank you and your company for helping us get rid of an unwanted timeshare. You all were very competent and professional in all of our dealings. The money is now in our bank and we are very glad to be out of the timeshare business. We especially want to thank you for your prompt response to our many questions concerning this transaction. We were in unknown territory and your guidance through the process was very comforting.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Lynda White</h3>
+            <p>
+            Thank you so much for your work on this purchase on our behalf. We were truly impressed with the speed and accuracy with which this was done. It was fairly effortless on our part, and although we were quite anxious about this purchase, mainly because we are from Canada and not sure how it would work, we were very pleasantly surprised on how well the whole transaction unfolded and very impressed on the professionalism with which it was done. Thank you again for your work on our behalf.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Mickey and Vickie Ashby</h3>
+            <p>
+            We have been impressed with your communication emails, letting us know what was going on as quickly as possible. We would strongly recommend you and TRCS, Inc. to any client and would certainly use TRCS, Inc. for any future transfers.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Dave Roach</h3>
+            <p>
+            The transaction went extremely smoothly. Thank you. I appreciated the high level of responsiveness that you and those in your organization provided. Any inquiry I had during the process was answered very quickly and thoroughly.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>M.G. Neithercutt</h3>
+            <p>
+            Thanks for your good work! We are experienced in timeshare transactions & found your services to be timely, efficient, and quite enjoyable.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Wesley Anderson</h3>
+            <p>
+            I was very appreciative of the efficiency and professionalism of TRCS. I didn’t really have to worry about a thing once all the documents were provided. I especially liked the online access that TRCS provided which allowed me to follow the progress of the transaction. I would recommend your services to anyone seeking to sell or purchase a timeshare.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Joanne Pernal</h3>
+            <p>
+            I want to thank you for the expedience of this transaction. I/we can not be anymore pleased than we are. Thank you for your service, and your being there to answer any question that we may have. Your customer service is very respected by us. Your Company is honest, sincere, and loyal.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>David Bea</h3>
+            <p>
+            TRCS did a great job of handling our Timeshare closing and staying in touch with us each step along the way. It's great to have a team of professionals you can count on handling your closing.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Chuck Henson</h3>
+            <p>
+            Thanks for your outstanding service. The closing was fast and efficient. TRCS, Inc. was also very flexible with the timing due to our being out of town a significant amount of the time. I would definitely use your services again if the opportunity presents itself.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Tula Watt</h3>
+            <p>
+            I would like to state here what superb smooth service I received from your Company and would recommend TRCS without hesitation to anyone looking for a competent closing company for timeshare transactions with wonderful well-trained staff.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Kevin and Judith Connelly</h3>
+            <p>
+            Judy and I were very impressed with the professionalism of your Organization. This was the smoothest, error free closing that I have ever experienced. Thank you for a job well done!
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Kenneth Singer</h3>
+            <p>
+            Thank you for taking care of our purchase in a timely and efficient manner. We are very pleased with the service.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>George and Vicki Hathaway</h3>
+            <p>
+            Many thanks to you and your staff, Karina. Your closing process was VERY efficient and extremely easy/smooth. No problems at all, and we are very satisfied with all aspects of your service. Job well done!!!
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Robert & Patricia Beattie</h3>
+            <p>
+            You have done an excellent job to make this purchase exactly what we wanted and at a great price. All this and as easy as some emails, faxes and a couple of registered letters. This deal could not have gone smoother. Thanks so much. Should we choose to purchase another time share it will definitely be through you.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Adriana Prata</h3>
+            <p>
+            Our experience with Karina and TRCS Inc. has been amazing! I buy and sell timeshares frequently, and I must say that Karina and TRCS Inc. have done the fastest and most professional job with regarding to our closing. Karina responded instantly by email to all of our questions, she sped up the processing and closing by expediting the documents and electronically recording the deed, and kept us informed at each stage in the process. I don't have enough words to express my satisfaction with the quality and speed of her work! Thank you, you have gained a repeat customer!
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Bob Thompson</h3>
+            <p>
+            Service was excellent, including understandable documents that were easily completed. Karina was responsive to my questions and inquiries. Received funds promptly when transfer occurred. I will use trcsinc again.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Joan Coffenberg</h3>
+            <p>
+            Thank you so much for your seamless and efficient handling of my timeshare closing. You made the entire process a pleasant and easy experience, something not always the case in real estate transactions. Because I was out of town and on extended vacation during the process, you facilitated my special needs of being away from my home office, and responded promptly to all my concerns. I applaud your professionalism, and thank you for your excellent service. Please feel free to share this with anyone as testament to this exceedingly efficient transaction.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Karen Butler</h3>
+            <p>
+            The service provided by TRCS was professional and fraud proof. The whole transaction was easy and we received our check faster than expected. I willl definitely use the service again and recommend to others.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Dina Mazzocco</h3>
+            <p>
+            This was such an easy transaction and everything moved very quickly without delays or hiccups. I want to thank you for making this a wonderful experience and truly enjoyed working with you and your company.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Robert T. Gorson Jr.</h3>
+            <p>
+            I have purchased several residential properties and four (4) timeshares over the past thirty (30) years and this experience with our real estate broker and your company has been the most pleasant, seamless and best customer service that I have ever received.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Benjamin Hall</h3>
+            <p>
+            I was extremely impressed with the customer service provided by TRCS, Inc. They made the timeshare purchase process stress-free. I would definitely use this service in the future.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Ed Reichardt</h3>
+            <p>
+            Thanks for the fast service and smooth transaction. I appreciated the clear instructions that accompanied the paperwork.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Marc B. - Sharon,MA</h3>
+            <p>
+            The service from time share marketing, sale through to the close was excellent. Made the process easy for all involved parties, would highly recommend.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="myCarousel">
+            <h3>Debbie Morea</h3>
+            <p>
+            Thank you for your efficient , timely and professional services! The ease of seeing where things stood online was reassuring but your quick and straightforward handling of the closing and my questions was even more important. I would recommend you and your services and wouldn't hesitate to use you again.
             </p>
           </div>
         </div>
